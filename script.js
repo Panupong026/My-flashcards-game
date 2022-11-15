726e4e20af24c3b0cbe87791b62970bd1396287b
panupong <panuponglovecal@gmail.com>
2022-11-15 16:11:13 +0700

    "shuffle"

--- script.js
+++ script.js
@@ -1,9 +1,77 @@
-const card = document.querySelectorAll("#card")
+const card = document.querySelectorAll(".cardItem")
+const image = document.querySelectorAll("img")
+const resetBtn = document.querySelector("#resetButton")
+
+
+let Flipped = false
+let firstCard, secondCard
+let countCorrect = 0
+let fixBug = false
+
+function shuffle () {
+    card.forEach(card => {
+        let random = Math.floor(Math.random() * 12)
+        card.style.order = random
+    })
+}
+shuffle()
+
 
 function flipCard () {
-    this.classList.toggle("flipCard")
+    if (fixBug) return
+
+    if (this === firstCard) return
+
+    this.classList.add("flipCard")
+    if (!Flipped) {
+        firstCard = this
+        Flipped = true
+    } else {
+        secondCard = this
+        Flipped = false
+        checking ()
+    }
+}
+
+
+// Check for Match
+function checking () {
+    // The cards is matched
+    if (firstCard.dataset.picno === secondCard.dataset.picno) {
+        firstCard.removeEventListener("click", flipCard)
+        secondCard.removeEventListener("click", flipCard)
+        
+        countCorrect ++
+        
+        // Display the alert
+        setTimeout (() => {
+            if (countCorrect === 4) {
+                    alert ("Well done")
+                }
+            }, 1500)
+        
+    } else {
+        // The cards is not matched
+        fixBug = true
+        setTimeout (() => {
+            firstCard.classList.remove("flipCard")
+            secondCard.classList.remove("flipCard")
+            
+            fixBug = false
+        }, 1000)
+    }
 }
 
 card.forEach((item) => {
     item.addEventListener("click", flipCard)
 })
+
+// Reset Button
+// resetBtn.addEventListener ("click", () => {
+//     card.forEach((card) => {
+//         this.classList.add("flipCard")
+//         card.addEventListener("click", flipCard)
+//         Flipped = false
+//         console.log("hi")
+//     })
+// })
